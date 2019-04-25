0ab8e8930924a353dc28914b4a5bac08cd7a52b9
ahmad dr <darvishi18@yahoo.com>
2019-04-26 02:05:32 +0430

    ini repo

---
+++ mellat-woocommerce/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: درگاه پرداخت بانک ملت افزونه ووکامرس
+Version: 4.1.0
+Description:  درگاه پرداخت بانک ملت برای افزونه ووکامرس
+Plugin URI: http://woocommerce.ir/
+Author: ووکامرس فارسی
+Author URI: http://woocommerce.ir/
+Contributors: Persianscript
+WC requires at least: 3.0.0
+WC tested up to: 3.5.2
+*/
+include_once("class-wc-gateway-bankmellat.php");
+//By HANNANStd in PersianScript.ir ==> woocommerce.ir
